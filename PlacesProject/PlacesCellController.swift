31d76e56a43f50d9852aa0e068e33758d006a7a0
VyakulinSA <VyakulinSA@gmail.com>
2020-08-13 22:57:44 +0400

    customCell

--- PlacesProject/PlacesCellController.swift
+++ PlacesProject/PlacesCellController.swift
@@ -15,16 +15,6 @@ class PlacesCellController: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
