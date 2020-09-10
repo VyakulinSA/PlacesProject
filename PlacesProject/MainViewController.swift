3aeac8d3fb63b2d0bc1961f6dca1e089c1b553e8
VyakulinSA <VyakulinSA@gmail.com>
2020-09-10 23:08:32 +0400

    add sorting

--- PlacesProject/MainViewController.swift
+++ PlacesProject/MainViewController.swift
@@ -9,9 +9,14 @@
 import RealmSwift
 import UIKit
 
-class MainViewController: UITableViewController {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     lazy var places = realm.objects(Place.self)
+    var ascendingSorting = true //создаем свойство для хранения информации, в каком направлении проводить сортировку(реверс)
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl! //Outlet для считывания SegmentedControl
+    @IBOutlet weak var reversedButton: UIBarButtonItem!//Outlet для считывания кнопки реверса
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,21 +26,22 @@ class MainViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         self.navigationItem.leftBarButtonItem = self.editButtonItem
+//         self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return places.isEmpty ? 0 : places.count
     }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PlacesCellController
         let place = places[indexPath.row]
+        cell.frame.size.height = 85
         
         cell.imageOutlet.image = UIImage(data: place.imageData!)
         cell.imageOutlet.layer.cornerRadius = cell.imageOutlet.frame.size.height / 2
@@ -49,42 +55,25 @@ class MainViewController: UITableViewController {
 
     
     // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
     
-    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
     
 
     
 //     Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             StorageManage.delObject(places[indexPath.row])
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-    
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -107,6 +96,36 @@ class MainViewController: UITableViewController {
             tableView.reloadData()
         }
     }
+    
+    @IBAction func sortSelection(_ sender: UISegmentedControl) { //создаем Action который выполняется при нажатии на segmentedControl
+        sortingPlace() //вызываем функцию сортировки
+    }
+    
+    @IBAction func reversedSorting(_ sender: Any) { //создаем Action который выполняется при нажатии на кнопку Реверса
+        
+        ascendingSorting.toggle() //изменяем направление реверса при каждом нажатии
+        
+        if ascendingSorting { //условия изменений реверса (меняем изображения)
+            reversedButton.image = #imageLiteral(resourceName: "AZ")
+        } else {
+            reversedButton.image = #imageLiteral(resourceName: "ZA")
+        }
+        
+        sortingPlace() //вызываем функцию сортировки
+        
+    }
+    
+    private func sortingPlace(){ //общая функция сортировки для SegmentedControl и Reversed
+        if segmentedControl.selectedSegmentIndex == 0 { //Если выбран первый пункт SegmentedControl
+            places = places.sorted(byKeyPath: "date", ascending: ascendingSorting) //массив с элементами из базы сортируем по дате и с условием выбранного реверса
+        } else {
+            places = places.sorted(byKeyPath: "name", ascending: ascendingSorting) // если другой элемент segmentedControl то по имени и в зависимости от значения реверса
+        }
+        
+        tableView.reloadData() //делаем обновление таблицы после сортировки
+    }
+    
+    
 }
 
 extension MainViewController {
